cd119ad1656a22f55913589e65f82fd85e834e0e
ferncot1 <67563876+giddy11@users.noreply.github.com>
2026-01-13 14:05:54 +0100

    updated

---
+++ january13th/section_1.js
@@ -0,0 +1,73 @@
+// Variables for storing data
+
+// I want to store the name of my sister in a variable
+
+// Three ways to declare a variables
+// 1. var
+// 2. let
+// 3. const
+
+var name = "Loveth"; // declaring and initializing a variable
+// Letters combined together form a string and because its a string, they are always enclosed in single or double quote.
+// The primitive type is string
+
+// console.log("Loveth") // "Loveth"
+// console.log(name) // name
+
+
+var age = 24; // Its primitive type is number
+
+console.log(age)
+
+var name = 700;
+
+// var stateoforigin = "Rivers State"
+
+var stateOfOrigin = "Rivers"; // Camel case naming
+var nameOfSchool = "SHcool of nursing"; // Camel case naming
+
+console.log(nameOfSchool)
+
+var MyAddress = "Ghana location"; // Pascal case
+var HeadOfDepartment = "Mrs Loveth"; // Pascal case
+
+var name_of_church = "Winners Chapel"; // snake case
+var name_of_country = "England"; // snake case
+
+// There are 3 ways of writing a variable name
+// 1. camel case
+// 2. pascal case
+// 3. snake case
+
+
+// There are examples of bad variables
+
+var age = "hello";
+
+//There are other Primitive types:
+// Primitive type boolean
+
+var isUserLoggedIn = false; // Primitive types boolean
+var isUserAbove20 = true; // //
+
+// Primitive type null
+var middleName = null; // Primitive type null
+
+// 1. Declare/Define a variable : var middleName
+// 2. Declare and Initialize a variable : var middleTown = "hi"
+
+// Primitive type undefined
+var lastLogin;
+
+// i  am a boy.
+// i am black in complexion.
+
+// Console.log: To print out the output of a statement
+
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+// var nameOfSister = "Loveth"
---
+++ january13th/section_2.js
@@ -0,0 +1,9 @@
+// OPERATORS
+// They are those mathematical operations been carried out
+// There are basically 4
+// 1. Arithmetic: +, -, *, /, %(modulus)
+// 2. Comparison: ==, ===, !=, !==, >, <, <=, >=
+// 3. Logical: &&, ||, !
+// 4. Assignment: =, +=, -=, *=, /=
+
+// Conditional statements

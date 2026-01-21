f27fc53b89b178e0528c0e6c2d054d2bde151746
ferncot1 <67563876+giddy11@users.noreply.github.com>
2026-01-21 13:39:49 +0100

    updated

---
+++ january19th/while.js
@@ -0,0 +1,19 @@
+/**
+ * WHile Loop
+ * Define  and initialize the variable
+ * while
+ * (
+ * The condition
+ * )
+ * {
+ * body
+ * increments
+ * }
+ */
+
+let i = 0;
+
+while(i <= 10){
+  console.log("Hello Loveth");
+  i++;
+}

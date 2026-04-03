5d79a7f188ad50211334b97c83b3bbbcc0dd5213
Edoghotu Gideon Azibaobuom <67563876+giddy11@users.noreply.github.com>
2026-04-03 14:12:05 +0100

    updated

---
+++ april/3rd/outline.js
@@ -0,0 +1,49 @@
+/**
+ * STEPS TO INSTALLING REACT
+ * ---------------
+ * ------------------------
+ * ctrl + j
+ * ctrl + b
+ * 
+ * PROJECT FILE & FOLDER DEFINITIONS
+ * package.json: - lists projects dependencies, scripts, and meta data
+ *               - controls how the project is run, built, and what packages are installed
+ * 
+ * package-lock.json: - records the exact versions of every installed package
+ *                    - ensures consistency installs across all environments
+ * 
+ * vite.config.js: - Configuration file for vite (the build tools)
+ *                 - Controls dev server, plugins, build options, etc
+ * 
+ * assets: - stores static files like images, SVGs, and other resources used in the app
+ *         - Usually found inside the src/assets or public/assets
+ * 
+ * README.md: - Documentation file for your project.
+ *            - Explains what the project does, how to install, run, and contribute.
+ * 
+ * src/: - 
+ * 
+ * public/:
+ * 
+ * 
+ * PROJECT STRUCTURE
+ * 
+ * JSX - WHAT IS JSX
+ * 
+ * JSX EXAMPLES
+ * 
+ * JSX RULES
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
